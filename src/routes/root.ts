35fe41a76baa5e158ade8aa10f643b9e7e1503b7
MageshBabu K <mage1k99@fossfreaks.com>
2022-08-17 14:09:51 +0530

    create a dummy post route

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -4,6 +4,14 @@ const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get('/', async function (request, reply) {
     return { root: true }
   })
+  // sending post request to this route will work as expected.
+  fastify.post("/passroute",async (request, reply) => {
+    return reply.send({
+      hi: "hello",
+    });
+  })
+  // when sending a post request with multipart/form-data body to a route that
+  // does not exist will throw an 415 error instead of 404.
 }
 
 export default root;
